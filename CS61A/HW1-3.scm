93f16417eddfaa765f7ec8c26bc2663bc467515e
Gan-Fall <ale.hdez.cruz@gmail.com>
2024-07-03 17:55:31 -0400

    Avoided using every in CS61A HW-1-3.scm

--- CS61A/HW1-3.scm
+++ CS61A/HW1-3.scm
@@ -1,3 +1,4 @@
+;MK I
 ;(define (switch sent)
 ;  (if (equal? 'You (first sent))
 ;       (switch-iter (sentence 'I (bf sent)))
@@ -9,11 +10,28 @@
 ;	((equal? (first sent) 'you) (switch-iter (sentence 'me (bf sent))))
 ;	(else '(Unknown Error))))
 
+;MK II
+;(define (switch-wd wd)
+;  (cond ((or (equal? wd 'I) (equal? wd 'me))'you)
+;	((equal? wd 'You) 'I)
+;	((equal? wd 'you) 'me)
+;	(else wd)))
+
+;(define (switch sent)
+;  (every switch-wd sent))
+
+;MK III
+(define (switch sent)
+  (if (or (equal? (first sent) 'You) (equal? (first sent) 'you))
+      (sentence 'I (switch-iter (bf sent)))
+      (switch-iter sent)))
+
+(define (switch-iter sent)
+  (if (not (empty? sent))
+      (sentence (switch-wd (first sent)) (switch-iter (bf sent)))
+      '()))
+
 (define (switch-wd wd)
-  (cond ((or (equal? wd 'I) (equal? wd 'me))'you)
-	((equal? wd 'You) 'I)
+  (cond ((or (equal? wd 'I) (equal? wd 'me)) 'you)
 	((equal? wd 'you) 'me)
 	(else wd)))
-
-(define (switch sent)
-  (every switch-wd sent))
