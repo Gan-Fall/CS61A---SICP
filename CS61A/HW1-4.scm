cb50c45a69230310b19558bef5d53434a62fc3e2
Gan-Fall <ale.hdez.cruz@gmail.com>
2024-07-03 23:10:38 -0400

    Made HW-1.4 recursive

--- CS61A/HW1-4.scm
+++ CS61A/HW1-4.scm
@@ -1,8 +1,13 @@
-(define (ordered? sent)
-  (if (empty? sent) #f (ordered-iter (first sent) (bf sent))))
+;(define (ordered? sent)
+;  (if (empty? sent) #f (ordered-iter (first sent) (bf sent))))
 
-(define (ordered-iter sent1 sent2)
-  (cond
-    ((empty? sent2) #t)
-    ((> sent1 (first sent2)) #f)
-    (else (ordered-iter (first sent2) (bf sent2)))))
+;(define (ordered-iter sent1 sent2)
+;  (cond
+;    ((empty? sent2) #t)
+;    ((> sent1 (first sent2)) #f)
+;    (else (ordered-iter (first sent2) (bf sent2)))))
+
+(define (ordered? sent)
+  (cond ((empty? (bf sent)) #t)
+	((<= (first sent) (first (bf sent))) (ordered? (bf sent)))
+	(else #f)))
