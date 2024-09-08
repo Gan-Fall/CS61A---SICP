89e28d34aeff60aa134c53b86b9dfa24c387978e
Gan-Fall <ale.hdez.cruz@gmail.com>
2024-09-08 19:33:58 -0400

    Some fixes to week 4 lab

--- labs/Week 4/week4-5.scm
+++ labs/Week 4/week4-5.scm
@@ -1,3 +1,9 @@
 (define (+rat a b)
    (make-rational (+ (numerator a) (numerator b))
                   (+ (denominator a) (denominator b))))
+
+;correct version
+(define (+rat a b)
+  (make-rational (+ (* (numerator a) (denominator b))
+		    (* (denominator a) (numerator b)))
+		 (* (denominator a) (denominator b))))
--- labs/Week 4/week4-8.scm
+++ labs/Week 4/week4-8.scm
@@ -3,9 +3,23 @@
       0
       (+ 1 (length (cdr items)))))
 
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) 
+                (- n 1))))
+
 (define (reverse items)
-  (define (helper iteration count iter-items)
-    (if (>= iteration count)
-      iter-items
-      (helper (+ 1 iteration) count (cons (cdr iter-items) (car iter-items))) ))
-  (helper 0 (length items) items))
+  (define (helper iteration)
+    (if (<= iteration 0)
+      ()
+      (cons (list-ref items (- iteration 1)) (helper (- iteration 1))) ))
+  (helper (length items)))
+
+;I'm dumb, here is a much better solution
+(define (reverse lst)
+  (define (iter old new)
+    (if (null? old)
+	new
+	(iter (cdr old) (cons (car old) new))))
+  (iter lst nil))
